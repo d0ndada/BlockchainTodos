58bf0aac3f095ddc8cf79322d8c33e2f3c8810a8
d0ndada <pintoavila09@gmail.com>
2023-04-05 15:20:06 +0200

    metmask change layout & load save connection

--- todos/src/components/Connect/ConnectMetamask.jsx
+++ todos/src/components/Connect/ConnectMetamask.jsx
@@ -1,37 +1,88 @@
-import React from 'react'
+import React from 'react';
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Web3 from 'web3';
 
 export const ConnectMetamask = () => {
-    const [account,setAccount] = useState();
-    
+    const [account, setAccount] = useState();
+    const [connected, setConnected] = useState(false);
 
-    const getAccounts = async () => {
-        if(window.ethereum) {
+
+    useEffect(() => {
+        getCurrentAccount();
+        addAccountListener();
+    }, []);
+
+    const connectToMetamask = async () => {
+        if (typeof window != "undefined" && typeof window.ethereum !="undefined") {
             try {
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                window.web3 = new Web3(window.ethereum);
-                const accounts = await window.web3.eth.getAccounts();
-                const currentAccount = accounts[0];
-                console.log("Current account:", currentAccount);
-                setAccount(currentAccount)
-
-        }catch (error) {
-            console.error("User denied account access")
-          }
-          } else {
+                // window.web3 = new Web3(window.ethereum);
+                const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                })
+                const account = accounts[0];
+                console.log("Current account:", account);
+                setAccount(account);
+                setConnected(true)
+            } catch (error) {
+                console.error("User denied account access")
+            }
+        } else {
             console.error('No Metamask detected')
-          }
-        };
-  return (
-    <div>
-        <button onClick={getAccounts} >
-            <h4>
+        }
+    };
+
+    
+    const getCurrentAccount = async () => {
+        if (typeof window != "undefined" && typeof window.ethereum !="undefined") {
+            try {
+                // window.web3 = new Web3(window.ethereum);
+                const accounts = await window.ethereum.request({
+                    method: "eth_accounts",
+                })
+                if (accounts.length > 0) {
+                    const account = accounts[0];
+                    console.log("Current account:", account);
+                    setAccount(account);
+                    setConnected(true)
+                } else {
+                    console.log("connect using button");
+                }
+               
+             
+            } catch (error) {
+                console.error("User denied account access")
+            }
+        } else {
+            console.error('No Metamask detected')
+        }
+    };
+    const addAccountListener = async () => {
+        if (typeof window != "undefined" && typeof window.ethereum !="undefined") {
+            window.ethereum.on("accountsChanged", (accounts) => {
+                setAccount(accounts[0])
+                console.log(accounts[0]);
+            })
+        } else {
+            setAccount("")
+            console.error('No Metamask detected')
+        }
+    };
+
+    
+    return (
+        <div>
+            {connected ? (
+                <h4>{`inlogged ${account}`}</h4>
+               
+            ):(
+                
+                <button onClick={connectToMetamask} >
                 <Icon icon="logos:metamask-icon" width="50" />
-                Connect with Metamask
-            </h4>
-        </button>
-    </div>
-  )
+                connnect
+            </button>
+            )
+            }
+        </div>
+    )
 }
